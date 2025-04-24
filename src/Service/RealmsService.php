064881ea8a8514f4d2d0695d45bc96bfe1dcf0ec
mainick <maico.orazio@gmail.com>
2025-04-24 17:34:57 +0200

    Manage realm roles

--- src/Service/RealmsService.php
+++ src/Service/RealmsService.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Mainick\KeycloakClientBundle\Service;
 
+use Mainick\KeycloakClientBundle\Representation\Collection\GroupCollection;
 use Mainick\KeycloakClientBundle\Representation\Collection\RealmCollection;
+use Mainick\KeycloakClientBundle\Representation\Collection\RoleCollection;
+use Mainick\KeycloakClientBundle\Representation\Collection\UserCollection;
+use Mainick\KeycloakClientBundle\Representation\GroupRepresentation;
 use Mainick\KeycloakClientBundle\Representation\RealmRepresentation;
+use Mainick\KeycloakClientBundle\Representation\RoleRepresentation;
+use Mainick\KeycloakClientBundle\Representation\UserRepresentation;
 
 final class RealmsService extends Service
 {
@@ -35,5 +41,49 @@ final class RealmsService extends Service
     public function delete(string $realm): bool
     {
         return $this->executeCommand(HttpMethodEnum::DELETE, 'admin/realms/'.$realm);
+    }
+
+    /**
+     * @return RoleCollection<RoleRepresentation>|null
+     */
+    public function roles(string $realm): ?RoleCollection
+    {
+        return $this->executeQuery('admin/realms/'.$realm.'/roles', RoleCollection::class);
+    }
+
+    public function role(string $realm, string $roleName): ?RoleRepresentation
+    {
+        return $this->executeQuery('admin/realms/'.$realm.'/roles/'.$roleName, RoleRepresentation::class);
+    }
+
+    public function createRole(string $realm, RoleRepresentation $role): bool
+    {
+        return $this->executeCommand(HttpMethodEnum::POST, 'admin/realms/'.$realm.'/roles', $role);
+    }
+
+    public function updateRole(string $realm, string $roleName, RoleRepresentation $role): bool
+    {
+        return $this->executeCommand(HttpMethodEnum::PUT, 'admin/realms/'.$realm.'/roles/'.$roleName, $role);
+    }
+
+    public function deleteRole(string $realm, string $roleName): bool
+    {
+        return $this->executeCommand(HttpMethodEnum::DELETE, 'admin/realms/'.$realm.'/roles/'.$roleName);
+    }
+
+    /**
+     * @return GroupCollection<GroupRepresentation>|null
+     */
+    public function getRoleGroups(string $realm, string $roleName, ?Criteria $criteria = null): ?GroupCollection
+    {
+        return $this->executeQuery('admin/realms/'.$realm.'/roles/'.$roleName.'/groups', GroupCollection::class, $criteria);
+    }
+
+    /**
+     * @return UserCollection<UserRepresentation>|null
+     */
+    public function getRoleUsers(string $realm, string $roleName, ?Criteria $criteria = null): ?UserCollection
+    {
+        return $this->executeQuery('admin/realms/'.$realm.'/roles/'.$roleName.'/users', UserCollection::class, $criteria);
     }
 }
