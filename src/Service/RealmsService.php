07c1f70dbb492b2c52ccfcb21ef2f06ca5656d88
mainick <maico.orazio@gmail.com>
2025-05-04 20:01:02 +0200

    duplicate methods

--- src/Service/RealmsService.php
+++ src/Service/RealmsService.php
@@ -42,48 +42,4 @@ final class RealmsService extends Service
     {
         return $this->executeCommand(HttpMethodEnum::DELETE, 'admin/realms/'.$realm);
     }
-
-    /**
-     * @return RoleCollection<RoleRepresentation>|null
-     */
-    public function roles(string $realm): ?RoleCollection
-    {
-        return $this->executeQuery('admin/realms/'.$realm.'/roles', RoleCollection::class);
-    }
-
-    public function role(string $realm, string $roleName): ?RoleRepresentation
-    {
-        return $this->executeQuery('admin/realms/'.$realm.'/roles/'.$roleName, RoleRepresentation::class);
-    }
-
-    public function createRole(string $realm, RoleRepresentation $role): bool
-    {
-        return $this->executeCommand(HttpMethodEnum::POST, 'admin/realms/'.$realm.'/roles', $role);
-    }
-
-    public function updateRole(string $realm, string $roleName, RoleRepresentation $role): bool
-    {
-        return $this->executeCommand(HttpMethodEnum::PUT, 'admin/realms/'.$realm.'/roles/'.$roleName, $role);
-    }
-
-    public function deleteRole(string $realm, string $roleName): bool
-    {
-        return $this->executeCommand(HttpMethodEnum::DELETE, 'admin/realms/'.$realm.'/roles/'.$roleName);
-    }
-
-    /**
-     * @return GroupCollection<GroupRepresentation>|null
-     */
-    public function getRoleGroups(string $realm, string $roleName, ?Criteria $criteria = null): ?GroupCollection
-    {
-        return $this->executeQuery('admin/realms/'.$realm.'/roles/'.$roleName.'/groups', GroupCollection::class, $criteria);
-    }
-
-    /**
-     * @return UserCollection<UserRepresentation>|null
-     */
-    public function getRoleUsers(string $realm, string $roleName, ?Criteria $criteria = null): ?UserCollection
-    {
-        return $this->executeQuery('admin/realms/'.$realm.'/roles/'.$roleName.'/users', UserCollection::class, $criteria);
-    }
 }
